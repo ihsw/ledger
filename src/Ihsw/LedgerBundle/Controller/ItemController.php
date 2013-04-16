d9f514ba942b17de73190d956893359e9b886c23
Adrian PArker <iamhiddensomewhere@gmail.com>
2013-04-15 23:09:34 -0400

    - changing tabs to spaces

--- src/Ihsw/LedgerBundle/Controller/ItemController.php
+++ src/Ihsw/LedgerBundle/Controller/ItemController.php
@@ -10,38 +10,38 @@ class ItemController extends Controller
 {
     public function indexAction()
     {
-    	// services
-    	$request = $this->get('request');
-    	$doctrine = $this->get('doctrine');
+        // services
+        $request = $this->get('request');
+        $doctrine = $this->get('doctrine');
 
-    	// repositories
-    	$em = $doctrine->getManager();
-    	$itemRepository = $em->getRepository('IhswLedgerBundle:Item');
+        // repositories
+        $em = $doctrine->getManager();
+        $itemRepository = $em->getRepository('IhswLedgerBundle:Item');
 
-    	// fetching items
-    	$items = $itemRepository->findAll();
+        // fetching items
+        $items = $itemRepository->findAll();
 
         return new JsonResponse($items);
     }
 
     public function createAction()
     {
-    	// services
-    	$request = $this->get('request');
-    	$doctrine = $this->get('doctrine');
+        // services
+        $request = $this->get('request');
+        $doctrine = $this->get('doctrine');
 
-    	// repositories
-    	$em = $doctrine->getManager();
-    	$itemRepository = $em->getRepository('IhswLedgerBundle:Item');
+        // repositories
+        $em = $doctrine->getManager();
+        $itemRepository = $em->getRepository('IhswLedgerBundle:Item');
 
-    	// inserting the item
-    	$content = json_decode($request->getContent(), true);
-    	$name = $content['name'];
-    	$item = new Item();
-    	$item->setName($name);
-    	$em->persist($item);
-    	$em->flush();
+        // inserting the item
+        $content = json_decode($request->getContent(), true);
+        $name = $content['name'];
+        $item = new Item();
+        $item->setName($name);
+        $em->persist($item);
+        $em->flush();
 
-    	return new JsonResponse($item);
+        return new JsonResponse($item);
     }
 }
